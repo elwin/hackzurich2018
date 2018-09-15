1a495a269656d04c68b6a9edcb84028aa73869da
Elwin Stephan <elwin.stephan@gmail.com>
2018-09-15 17:25:31 +0200

    Now with mockdata

--- database/factories/SegmentFactory.php
+++ database/factories/SegmentFactory.php
@@ -6,7 +6,10 @@ $factory->define(App\Segment::class, function (Faker $faker) {
     return [
         'streetname' => $faker->streetName,
         'score' => $faker->randomNumber,
-        'polyline' => [{"latitude": 1}],
+        'polyline' => [
+            ["latitude" => 51.50997, "longitude" => -0.13497],
+            ["latitude" => 51.51, "longitude" => -0.135]
+        ],
         'origin_latitude' => 1.2,
         'origin_longitude' => 2.3,
         'destination_latitude' => 3.4,
